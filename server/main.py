40df1851dc0274713da1fbc25995b0a5265bc230
inbarmiran <inbar.miran@campus.technion.ac.il>
2025-05-10 22:00:21 +0300

    erased debug mode and enabled input for PORT

--- server/main.py
+++ server/main.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the Flask server.')
     parser.add_argument('--port', type=int, default=5000)
     args = parser.parse_args()
-    app.run(debug=True, host='127.0.0.1', port=5000)
+ 
+    app.run(host='127.0.0.1', port=args.port)
